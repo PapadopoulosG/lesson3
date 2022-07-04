2b7f4e28e7d1b9011b0f821388c08f3cc267fe8e
Georgios Papadopoulos <linkin2895@gmail.com>
2022-07-04 11:46:09 +0100

    my implementation

--- src/inheritanceExample/Student.java
+++ src/inheritanceExample/Student.java
@@ -1,2 +1,25 @@
-package inheritanceExample;public class Student {
+package inheritanceExample;
+
+public class Student extends Person {
+    private String studentId;
+
+    public Student(String firstName,String lastName, String StudentId){
+        super(firstName,lastName);
+        this.studentId = StudentId;
+
+    }
+    public String getStudentId(){
+        return studentId;
+
+    }
+    public void setStudentId(String studentId)
+    {
+        this.studentId = studentId;
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.toString()+"Student Id is: "+ studentId;
+    }
 }
--- src/inheritanceExample/StudentEmployee.java
+++ src/inheritanceExample/StudentEmployee.java
@@ -1,2 +1,36 @@
-package inheritanceExample;public class StudentEmployee {
+package inheritanceExample;
+
+public class StudentEmployee extends Student{
+    private double rateOfPayPerHour;
+    private String employeeId;
+
+    public StudentEmployee(String firstName, String lastName, String studentId, Double rateofPayPerHour, String employeeId){
+        super(firstName,lastName,studentId);
+        this.rateOfPayPerHour = rateofPayPerHour;
+        this.employeeId = employeeId;
+
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+    public void setRateOfPayPerHour(Double rateOfPayPerHour){
+        this.rateOfPayPerHour = rateOfPayPerHour;
+
+    }
+
+    public String getEmployeeId(){
+        return employeeId;
+    }
+    public Double getRateOfPayPerHour(){
+        return rateOfPayPerHour;
+
+    }
+
+    @Override
+    public String toString(){
+        return super.toString()+" pay is "+rateOfPayPerHour+" and employee ID is: "+employeeId;
+
+    }
+
 }
